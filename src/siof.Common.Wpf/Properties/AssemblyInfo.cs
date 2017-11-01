372c2996009fbd2a4d9d811d970e2d24250c03d6
siof <siof@siof.pl>
2017-11-01 12:05:11 +0100

    Dependencies

--- src/siof.Common.Wpf/Properties/AssemblyInfo.cs
+++ src/siof.Common.Wpf/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows.Markup;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("siof.Common.Wpf")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library with my commonly used classes in MVVM projects")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("siof.Common.Wpf")]
@@ -36,5 +36,5 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
